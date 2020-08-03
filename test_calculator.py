09d41a48961b51f763fd5eb1b9343099f50758ae
Rajnish Kumar <rajjnish@gmail.com>
2020-08-03 23:25:03 +0200

    added function and modified test

--- test_calculator.py
+++ test_calculator.py
@@ -17,5 +17,5 @@ class TestCalculator:
         assert 100 == calculator.multiply(10, 10)
 
     def test_division(self):
-        assert 20 == calculator.divide(10, 2)
+        assert 5 == calculator.divide(10, 2)
 
